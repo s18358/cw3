6de2223259fe9cfb710139088e0ceaa939286d70
s18358 <s18358@pjwstk.edu.pl>
2020-03-18 23:30:11 +0100

    zad 4

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        public string GetStudents()
+        {
+            return "Kowalski, Nowak";
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStudenst(int id)
+        {
+            if(id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Nowak");
+            }
+
+            return NotFound("Nie ma studenta");
+        }
+    }
+}
